6441b86c018f2f6003557d700e9f57104a75eca5
Jay <8x15yz@gmail.com>
2022-09-15 23:29:33 +0900

    feat: 🎸 create cafe list crawling code
    카페리스트 크롤링 코드 작성

--- 0914hyunjukim/cafe_list_crawl.py
+++ 0914hyunjukim/cafe_list_crawl.py
@@ -3,6 +3,7 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
 import csv
 from tqdm import tqdm
 
@@ -14,21 +15,27 @@ url = 'https://map.kakao.com/'
 driver.get(url)
 
 elem = driver.find_element(By.ID, 'search.keyword.query')
-elem.send_keys("보드")
+elem.send_keys("보드카페")
 elem.send_keys("\n")
 
 time.sleep(2)
 
-lists = driver.find_element(By.ID, 'info.search.place.list')
+# lists = driver.find_element(By.ID, 'info.search.place.list')
+lists = driver.find_element(By.CSS_SELECTOR, 'ul')
+print(lists.text)
 
-li = lists.find_element(By.CSS_SELECTOR, 'li')
+li = lists.find_elements(By.CSS_SELECTOR, 'li')
+for i in li:
+    head_item = i.find_element(By.CLASS_NAME, 'head_item')
+    # print(head_item)
 
-li.find_element(By.CLASS_NAME, 'head_item').find_element(By.CSS_SELECTOR, 'span').send_keys(Keys.ENTER)
+# boardCafe = li.find_element(By.CLASS_NAME, 'head_item').find_element(By.CSS_SELECTOR, 'span')
+
+# ActionChains(driver).move_to_element(boardCafe).click()
 
 
 
 
 
 
-# file = open("cafe_list.csv", "w", newline='')
-# cafe_list = csv.writer(file)
+
