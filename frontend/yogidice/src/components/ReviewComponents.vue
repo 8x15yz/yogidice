c03e34b69277365751c76e1b12110aff16dd21d6
Jay <8x15yz@gmail.com>
2022-09-23 16:49:14 +0900

    perf: ⚡️ frontend ing

--- frontend/yogidice/src/components/ReviewComponents.vue
+++ frontend/yogidice/src/components/ReviewComponents.vue
@@ -1,26 +1,31 @@
 <template>
-    <div class="review-component-base">
-        {{reviewdatum}}
+    <div v-for="data in datum" :key="data.key">
+        나오고는 있오ㅓ?
+        {{datum}}
+        <div>
+            {{data.title_kr}} {{}}
+        </div>
     </div>
 </template>
 
 <script>
-// // import { toRefs } from '@vue/runtime-core'
-// export default{
-//     props: {
-//         reviewdatum: Array,
-//     },
-//     setup(props) {
-//         // const { reviewdatum } = toRefs(props.reviewdatum)
-//         return {
-            
-//         }
-//     },
-// }
+import { toRefs } from '@vue/runtime-core'
+export default{
+    props: {
+        reviewdatum: Array,
+    },
+    setup(props) {
+        const { datum } = toRefs(props.reviewdatum)
+        console.log(datum)
+        return {
+            datum
+        }
+    },
+}
 </script>
 
 <style>
-.review-component-base {
+#review-component-base {
     background-color: pink;
     height: 90px;
     border: 3px blue solid;
