38373e43be2b6e286ad5338f62011669b3c9b8e8
wonjaechoi <wj5295@naver.com>
2022-09-26 23:57:26 +0900

    feat: 🎸 update Loading Spinner component

--- frontend/yogidice/src/components/spinner/LoadingSpinner.vue
+++ frontend/yogidice/src/components/spinner/LoadingSpinner.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="spinner-container">
-    <div class="spinner" />
+   <div class="spinner">
+    <div class="spinner-inside">
+    </div>
+   </div>
   </div>
+ 
 </template>
 
 <script>
@@ -11,18 +15,48 @@ export default {};
 <style scoped>
 .spinner-container {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 240px;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-content: center;
+  flex-wrap: nowrap;
+  overflow: scroll;
+  gap: 2vw;
+  width: 100%;
+  height: 80vh;
+  justify-content : center;
+  overflow: scroll;
 }
-.spinner {
+.spinner{
+  position:absolute;
+  left:45%;
+    top:40%;
+  color: white;
   width: 50px;
   height: 50px;
-  border-radius: 50%;
-  border: 5px solid #e0e0e0;
-  border-bottom: 5px solid #fe9616;
+  text-align: center;
+  
+  font-size: 20px;
+  border-radius: 50px;
+  background: white;
+ 
+}
+.spinner-inside{
+    margin-top:15%;
+    margin-left: 15%;
+    width: 30px;
+    height: 30px;
+    background-image: url("@/static/loadingspinner30.gif");
+}
+/* .spinner-container {
+  display: inline-block;
+  position: absolute;
+  width: 64px;
+  height: 64px;
+  top: 47%;
+  left: 47%;
   background-image: url("@/static/loadingspinner50.gif");
   background-repeat: no-repeat;
   position: relative;
-}
+} */
+
 </style>
