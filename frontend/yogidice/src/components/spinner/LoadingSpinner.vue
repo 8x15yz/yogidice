259fb42fa2af0c703c09775f849f9dfbb2b65231
wonjaechoi <wj5295@naver.com>
2022-09-26 17:07:23 +0900

    feat: 🎸 feat Loading Spinner

---
+++ frontend/yogidice/src/components/spinner/LoadingSpinner.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="spinner-container">
+    <div class="spinner" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.spinner-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 240px;
+}
+.spinner {
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  border: 5px solid #e0e0e0;
+  border-bottom: 5px solid #fe9616;
+  background-image: url("@/static/loadingspinner50.gif");
+  background-repeat: no-repeat;
+  position: relative;
+}
+</style>
