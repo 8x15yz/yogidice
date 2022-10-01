958a25ab9c77ae963d175a9ba3e6dec7c7410cd6
neweastK <eastshin2021@gmail.com>
2022-10-01 15:56:15 +0900

    feat: 🎸 penalty page create
    Former-commit-id: 1119508a8376cc3bbb166f63e7491d2328a8d7a6

---
+++ frontend/yogidice/src/components/plusgame/GameClock.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div v-show="showStopWatch">
+      <stop-watch ></stop-watch>
+    </div>
+    <div v-show="showTimer">
+      <game-timer></game-timer>
+    </div>
+    <div class="timer-choice">
+      <div @click="toStopWatch" class="button-small-blue text-button">스톱워치</div>
+      <div @click="toTimer" class="button-small-blue text-button">타이머</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import StopWatch from "@/components/plusgame/StopWatch.vue"
+import GameTimer from "@/components/plusgame/GameTimer.vue"
+import { ref } from "vue"
+export default {
+  components: {
+    StopWatch,
+    GameTimer
+  },
+  setup(){
+    let showStopWatch = ref(true)
+    let showTimer = ref(false)
+    
+    const toStopWatch = function () {
+      if (showStopWatch.value === false) {
+      showStopWatch.value = true
+      console.log(showStopWatch.value)
+      showTimer.value = false
+    }}
+    const toTimer = function () {
+      if (showTimer.value === false) {
+      showStopWatch.value = false
+      showTimer.value = true
+    }}
+
+    return {
+      showStopWatch,
+      showTimer,
+      toTimer,
+      toStopWatch
+    }
+  }
+
+}
+</script>
+
+<style>
+.timer-choice {
+  display: flex;
+  justify-content: center;
+  gap: 2vw;
+  margin-top: 4vh;
+}
+
+</style>
