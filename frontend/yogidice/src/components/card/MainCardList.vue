99b814b66f8ab4392388346f72465ed8ff9f1617
neweastK <eastshin2021@gmail.com>
2022-10-05 15:31:00 +0900

    fix long card err
    Former-commit-id: 09adf22cf6a7e240ba64f8ada5493116d42e38ea

--- frontend/yogidice/src/components/card/MainCardList.vue
+++ frontend/yogidice/src/components/card/MainCardList.vue
@@ -34,7 +34,6 @@ export default {
     const showDetail = function(n) {
       router.push({name:"GameDetail", query:{"gameId":n.id, "title":n.titleKr}})
       store.dispatch("games/getDetails",n.id,)
-      console.log('이거이고이거', n.id)
     }
     let gameLists = computed(()=>store.state.games.mainGames)
     let gameType = computed(()=>store.state.games.presentType)
